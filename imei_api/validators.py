4673d8ef08b247f727baa005325f48944a84b057
Михайлов Артём <artem.mikh100@gmail.com>
2025-01-30 11:42:08 +0300

    Add luhn check imei

--- imei_api/validators.py
+++ imei_api/validators.py
@@ -62,12 +62,34 @@ async def check_imei_correct(imei: str) -> str or HTTPException:
             status_code=400,
             detail="IMEI должен содержать ровно 15 цифр"
         )
+    if not luhn_check(imei):
+        raise HTTPException(
+            status_code=400,
+            detail="Некорректный IMEI, проверьте данные"
+        )
+
     return imei
+
+
+def luhn_check(imei: str) -> bool:
+    """Проверяет IMEI с использованием алгоритма Luhn."""
+    digits = [int(d) for d in imei]
+    checksum = 0
+
+    for i, digit in enumerate(reversed(digits)):
+        if i % 2 == 1:  # Удваиваем каждую вторую цифру
+            digit *= 2
+            if digit > 9:  # Если больше 9, складываем цифры
+                digit -= 9
+        checksum += digit
+
+    return checksum % 10 == 0
 
 
 async def check_token_exists(
         session: AsyncSession, token) -> None or HTTPException:
     """Проверяет, существует ли пользователь с переданным token в базе."""
+
     user: User = await user_crud.get_user_obj_by_token(session, token)
     if user is None:
         raise HTTPException(
